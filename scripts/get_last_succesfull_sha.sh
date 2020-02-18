f29a9a16bd8bc698653e0c1c9c496658d4fbd238
floris.janssens <floris.janssens@student.kuleuven.be>
2020-02-18 22:39:47 +0100

    Add tested Travis Pipeline and required files (explanation in .travis.yml)

---
+++ scripts/get_last_succesfull_sha.sh
@@ -0,0 +1,39 @@
+#-- copyright
+# OpenProject user and project management plugin.
+# Copyright (C) 2020 Floris Janssens (florisjanssens@outlook.com)
+#
+# OpenProject is an open source project management software.
+# Copyright (C) 2012-2020 the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2017 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See doc/COPYRIGHT.md for more details.
+#++
+
+#!/bin/bash
+test=$(curl -s -H "Authorization: token $GITHUB_TOKEN" 'https://api.github.com/repos/opf/openproject/commits?sha=dev&per_page=200'| jq '.[] | {sha: .sha}')
+
+for row in $(echo "${test}" | jq -r '.[]'); do
+    match=$(curl -s -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/opf/openproject/commits/${row}/status | jq -r '. | select(.statuses[].context|test("continuous-integration/travis-ci/.")) | select(.state|test("success")).sha')
+    
+    if [ ! -z ${match//\'+x} ]; then echo "${match}" && break; fi
+done
